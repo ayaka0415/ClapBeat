6a0ba56b0d29a78785f4bd53236d919f6914fee3
Owner <owner@MA3011.local>
2020-06-08 17:03:28 +0900

    Alj100M

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Owner on 2020/06/05.
+//  Copyright © 2020 ALJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
